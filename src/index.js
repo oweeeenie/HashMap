f3f89cb6efe698b2eba4a8faa6d7fd85bcb44478
oweeeenie <owensullivan419@yahoo.com>
2024-12-08 22:31:10 -0500

    didnt write too much code, but got a TON of clarification on a ton of things, back at this later

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './styles.css';
+
+class HashMap {
+  constructor(capacity, loadFactor) {
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+    this.buckets = new Array(this.capacity);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+    let hashCode = hash(key);
+    hashCode = hashCode % this.capacity;
+
+    if (!this.buckets[hashCode]) {
+      this.buckets[hashCode] = [];
+      this.buckets[hashCode].push({ key, value });
+    }
+  }
+}
