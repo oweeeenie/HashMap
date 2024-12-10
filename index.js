edbcfa4a2bfae975d398e780be060514b6252e75
oweeeenie <owensullivan419@yahoo.com>
2024-12-09 21:58:11 -0500

    added has(key) functionality & remove(key)

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class HashMap {
 
   set(key, value) {
     let hashCode = this.hash(key);
-    hashCode = Math.abs(hashCode) % this.capacity; // makes hashcode POSITIVE ALWAYS
+    hashCode = Math.abs(hashCode) % this.capacity; // hashcode POSITIVE ALWAYS
 
     if (!this.buckets[hashCode]) {
       // checks if the array index is already taken
@@ -54,6 +54,39 @@ class HashMap {
     }
     return undefined;
   }
+
+  has(key) {
+    let hashCode = this.hash(key);
+    hashCode = Math.abs(hashCode) % this.capacity;
+
+    if (!this.buckets[hashCode]) {
+      return false;
+    }
+
+    for (let i = 0; i < this.buckets[hashCode].length; i++) {
+      if (key === this.buckets[hashCode][i].key) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(key) {
+    let hashCode = this.hash(key);
+    hashCode = Math.abs(hashCode) % this.capacity;
+
+    if (!this.buckets[hashCode]) {
+      return false;
+    }
+
+    for (let i = 0; i < this.buckets[hashCode].length; i++) {
+      if (key === this.buckets[hashCode][i].key) {
+        this.buckets[hashCode].splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 const hashmap = new HashMap();
@@ -65,4 +98,14 @@ hashmap.set('c', 3);
 console.log(hashmap.get('a')); // should print: 1
 console.log(hashmap.get('b')); // should print: 2
 console.log(hashmap.get('c')); // should print: 3
-console.log(hashmap.get('d')); // should print: undefined (key "d" doesn't exist)
+
+// Remove a key that exists
+console.log(hashmap.remove('b')); // should print: true
+console.log(hashmap.get('b')); // should print: undefined (because 'b' was removed)
+
+// Try to remove a key that doesn't exist
+console.log(hashmap.remove('z')); // should print: false
+console.log(hashmap.get('z')); // should print: undefined (because 'z' was never added)
+
+console.log(hashmap.get('a')); // should still print: 1
+console.log(hashmap.get('c')); // should still print: 3
